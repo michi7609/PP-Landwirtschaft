54ecabe21b48287526a8ee59483e14d46ed5ea53
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2022-12-30 21:14:47 +0100

    Add front layout

--- app/src/main/java/com/jochengehtab/projektprsentationlandwirtschaft/MainActivity.java
+++ app/src/main/java/com/jochengehtab/projektprsentationlandwirtschaft/MainActivity.java
@@ -1,8 +1,9 @@
 package com.jochengehtab.projektprsentationlandwirtschaft;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,10 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageButton elektrotraktor = findViewById(R.id.elektrotraktor),
+                alternativemotoren = findViewById(R.id.alternativemotoren),
+                emotor = findViewById(R.id.emotor),
+                synthetischerkraftstoff = findViewById(R.id.synthetischerkraftstoff),
+                erneuerbareenergien = findViewById(R.id.erneuerbareenergien),
+                umruestung = findViewById(R.id.umruestung),
+                landwirt = findViewById(R.id.landwirt),
+                mensch = findViewById(R.id.mensch);
 
-        ImageButton imageButton = findViewById(R.id.back);
-        imageButton.setOnClickListener(v -> {
+        alternativemotoren.setOnClickListener(v -> {});
+        emotor.setOnClickListener(v -> {});
+        synthetischerkraftstoff.setOnClickListener(v -> {});
+        erneuerbareenergien.setOnClickListener(v -> {});
+        umruestung.setOnClickListener(v -> {});
+        landwirt.setOnClickListener(v -> {});
+        mensch.setOnClickListener(v -> {});
+        elektrotraktor.setOnClickListener(v -> {});
 
-        });
+        Button button = findViewById(R.id.src);
+        button.setOnClickListener(v -> startActivity(new Intent(this, Src.class)));
     }
 }
