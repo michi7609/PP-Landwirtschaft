d99c7409cc38ba21e79f8ef3f9b0c1029a6e80dd
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2023-01-01 16:36:29 +0100

    Design Cleanup

--- app/src/main/java/com/jochengehtab/projektprsentationlandwirtschaft/Src.java
+++ app/src/main/java/com/jochengehtab/projektprsentationlandwirtschaft/Src.java
@@ -2,6 +2,8 @@ package com.jochengehtab.projektprsentationlandwirtschaft;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class Src extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class Src extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_src);
+
+        TextView firstText = findViewById(R.id.firstsrc),
+                secondText = findViewById(R.id.secondsrc),
+                thirdText = findViewById(R.id.thirdsrc),
+                fourthText = findViewById(R.id.fourthsrc),
+                fifthText = findViewById(R.id.fifthsrc),
+                sixthText = findViewById(R.id.sixthsrc),
+                seventhText = findViewById(R.id.seventhsrc),
+                eightText = findViewById(R.id.eightsrc);
+
+        firstText.setMovementMethod(LinkMovementMethod.getInstance());
+        secondText.setMovementMethod(LinkMovementMethod.getInstance());
+        thirdText.setMovementMethod(LinkMovementMethod.getInstance());
+        fourthText.setMovementMethod(LinkMovementMethod.getInstance());
+        fifthText.setMovementMethod(LinkMovementMethod.getInstance());
+        sixthText.setMovementMethod(LinkMovementMethod.getInstance());
+        seventhText.setMovementMethod(LinkMovementMethod.getInstance());
+        eightText.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
